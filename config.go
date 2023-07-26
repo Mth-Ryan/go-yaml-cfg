c6b3dd0cbf080662fca5dc1ce5a03221d733f282
Mateus Ryan <mthryan@protonmail.com>
2023-07-26 17:44:33 -0300

    Optinal singleton pattern

--- config.go
+++ config.go
@@ -19,19 +19,28 @@ type config struct {
 
 var singleton config
 
+// InitializeConfigSingleton will create a instance of the configuration
+// to be fetched with GetConfigFromSingleton or MustGetConfigFromSingleton.
+// Use this when you are not using a DI system.
 func InitializeConfigSingleton[T Instance](path string) error {
-	singleton.mu.Lock()
-	defer singleton.mu.Unlock()
+	if singleton.data == nil {
+		singleton.mu.Lock()
+		defer singleton.mu.Unlock()
 
-	target := new(T)
-	loadAndParseConfig(path, target)
-	replaceEnvOnTarget(target)
-	singleton.ty = reflect.TypeOf(*target)
-	singleton.data = interface{}(*target)
+		target, err := GetConfig[T](path)
+		if err != nil {
+			return err
+		}
+
+		singleton.ty = reflect.TypeOf(target)
+		singleton.data = interface{}(target)
+	}
 
 	return nil
 }
 
+// MustInitializeConfigSingleton has the same behaviour of InitializeConfigSingleton,
+// but will panic if an error occurs.
 func MustInitializeConfigSingleton[T Instance](path string) {
 	err := InitializeConfigSingleton[T](path)
 	if err != nil {
@@ -39,7 +48,10 @@ func MustInitializeConfigSingleton[T Instance](path string) {
 	}
 }
 
-func GetConfig[T Instance]() (T, error) {
+// GetConfigFromSingleton will only return a configuration with type T if:
+// 1) The singleton has been already initialized.
+// 2) The type of the generic is the same of the singleton initialization.
+func GetConfigFromSingleton[T Instance]() (T, error) {
 	if singleton.data != nil {
 		if singleton.ty != reflect.TypeOf(*new(T)) {
 			return *new(T), fmt.Errorf("mismatch generic type: the given type is diferent from the singleton data")
@@ -50,12 +62,36 @@ func GetConfig[T Instance]() (T, error) {
 	}
 }
 
-func MustGetConfig[T Instance]() T {
-	conf, err := GetConfig[T]()
+// MustGetConfigFromSingleton has the same behaviour of GetConfigFromSingleton
+// but will panic if an error occurs.
+func MustGetConfigFromSingleton[T Instance]() T {
+	conf, err := GetConfigFromSingleton[T]()
 	if err != nil {
 		panic(err)
 	}
 	return conf
+}
+
+// GetConfig will always return a new config instance with the type T if the
+// read file and the parsing stages was successful.
+func GetConfig[T Instance](path string) (T, error) {
+	target := new(T)
+	err := loadAndParseConfig(path, target)
+	if err != nil {
+		return *target, err
+	}
+	replaceEnvOnTarget(target)
+	return *target, nil
+}
+
+// MustGetConfig has the same behaviour of GetConfig but panics
+// when an error happens.
+func MustGetConfig[T Instance](path string) T {
+	target, err := GetConfig[T](path)
+	if err != nil {
+		panic(err)
+	}
+	return target
 }
 
 func loadAndParseConfig[T Instance](path string, target *T) error {
